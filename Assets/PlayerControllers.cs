3068932d04013b389a9f0ab97f5bd2ba1ef80410
Jedd Morgan <jeddrmorgan@gmail.com>
2020-06-26 18:40:47 +0000

    Triangle

--- Assets/PlayerControllers.cs
+++ Assets/PlayerControllers.cs
@@ -4,28 +4,36 @@ using UnityEngine;
 
 public class PlayerControllers : MonoBehaviour
 {
-
     [SerializeField]
     private GameObject partner;
+
+    [SerializeField]
+    private bool lockable = false;
 
     [SerializeField]
     private float attractionSpeed = 10f;
 
 
+
     public void FixedUpdate()
     {
         var rb = this.GetComponent<Rigidbody2D>();
-        Vector3 force = Vector3.zero;
 
-        if(Input.GetKey(KeyCode.W))
+        if (!lockable || !Input.GetKey(KeyCode.Space))
         {
-            force = partner.transform.position - this.transform.position;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            force = this.transform.position - partner.transform.position;
-        }
+            Vector3 force = Vector3.zero;
 
-        rb.AddForce(force.normalized * attractionSpeed);
+            if (Input.GetKey(KeyCode.W))
+            {
+                force = partner.transform.position - this.transform.position;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                force = this.transform.position - partner.transform.position;
+            }
+
+            rb.AddForce(force.normalized * attractionSpeed);
+        }
+        
     }
 }
