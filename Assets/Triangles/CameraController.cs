bda33b1be0bd41247636211b1c6e4481f516f0e5
Jedd Morgan <jeddrmorgan@gmail.com>
2020-06-27 21:45:54 +0000

    More to the level

--- Assets/Triangles/CameraController.cs
+++ Assets/Triangles/CameraController.cs
@@ -14,14 +14,16 @@ public class CameraController : MonoBehaviour
     [Range(0,1)]
     private float smoothAmount;
 
-    private void LateUpdate()
+    private void Update()
     {
-
         Vector3 diff = (triangle1.transform.position - this.transform.position).normalized;
 
         float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+    }
 
+    private void LateUpdate()
+    {
 
         Vector3 midPoint = Vector3.Lerp(triangle1.transform.position, triangle2.transform.position, 0.5f);
         transform.position = Vector3.Lerp(transform.position, new Vector3(midPoint.x, midPoint.y, -20), smoothAmount * 10 * Time.deltaTime);
