ef0e9e32388528d165281d1bf38fcbbad67b6211
Jedd Morgan <jeddrmorgan@gmail.com>
2020-06-28 01:51:12 +0000

    Added Menu

---
+++ Assets/Main Menu/UIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIController: MonoBehaviour
+{
+    /// <summary>
+    /// Load next scene in the build index
+    /// </summary>
+    public void NewGame()
+    {
+        Debug.Log("Loading new scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>
+    /// Exit the game
+    /// </summary>
+    public void ExitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
